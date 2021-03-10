00c66880fe799343b143694505b073ff10cbf126
leoramos182 <leo.ramos182@hotmail.com>
2021-03-10 16:18:10 -0300

    Adicionado arquivo de menu Implimentado um toast quando o menu é clicado

--- app/src/main/java/com/example/contatosrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/contatosrecyclerview/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.contatosrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.contato_item.*
@@ -31,10 +35,32 @@ class MainActivity : AppCompatActivity() {
             "img.jpg"
         ),
              Contato(
-                 "Joao Silva",
+                 "Carlos Silva",
                  "(21) 96724-8664",
                  "img.jpg"
                 )
             ))
+    }
+    private fun toastMessage(message : String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+    override fun onCreateOptionsMenu(menu : Menu?) : Boolean{
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_menu_um -> {
+                toastMessage("Menu um tocado!")
+                true
+            }
+            R.id.menu_dois -> {
+                toastMessage("Menu dois tocado!")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
